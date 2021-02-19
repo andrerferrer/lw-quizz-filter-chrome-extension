8db5aed91b6ad5678f2067a783bffa845e99f71a
andre ferrer <andrerferrer@hotmail.com>
2021-02-19 11:53:24 -0300

    Add finished filter

--- background.js
+++ background.js
@@ -1,7 +1,7 @@
 chrome.browserAction.onClicked.addListener((tab) => {
   chrome.tabs.executeScript({
     code: `
-    	const filterTables=()=>{const t=document.querySelector("body > div > div:nth-child(3) > div > div:nth-child(1)");if(t){const e='\n\t\t\t<div class="m-3">\n\t\t\t\t<label for="filter">Which status are you looking for?</label>\n\t\t\t\t<select id="filter">\n\t\t\t\t  <option value="Started">Both</option>\n\t\t\t\t  <option value="Not Started">Not Started</option>\n\t\t\t\t</select>\n\t\t\t</div>\n\t\t',n=t=>{const e=t.currentTarget.value,n=new RegExp(e,"i");document.querySelectorAll("tbody .quiz-row").forEach(t=>{const e=t.querySelector("td.quiz-status").innerText;t.style.display=n.test(e)?"":"none"})};t.insertAdjacentHTML("afterbegin",e),document.getElementById("filter").addEventListener("change",n)}};filterTables();
+    	const filterTables=()=>{const t=document.querySelector("body > div > div:nth-child(3) > div > div:nth-child(1)");if(t){const e='<div class="m-3"><label for="filter">Which status are you looking for?</label><select id="filter">  <option value="Started">All</option>  <option value="Not Started">Not Started</option>  <option value="Finished">Finished</option></select></div>',n=t=>{const e=t.currentTarget.value,n=document.querySelectorAll(".table tbody .quiz-row");if("Finished"==e)n.forEach(t=>{const e=t.querySelector("td:nth-child(2)").innerText;t.style.display=/-/.test(e)?"none":""});else{const t=new RegExp(e,"i");n.forEach(e=>{const n=e.querySelector("td.quiz-status").innerText;e.style.display=t.test(n)?"":"none"})}};t.insertAdjacentHTML("afterbegin",e),document.getElementById("filter").addEventListener("change",n)}};filterTables();
     `
   });
 });
--- not_ugly.js
+++ not_ugly.js
@@ -6,20 +6,29 @@ const filterTables = () => {
 			<div class="m-3">
 				<label for="filter">Which status are you looking for?</label>
 				<select id="filter">
-				  <option value="Started">Both</option>
+				  <option value="Started">All</option>
 				  <option value="Not Started">Not Started</option>
+				  <option value="Finished">Finished</option>
 				</select>
 			</div>
 		`
 
+		// Need to refactor this
 		const filterTables = (event) => {
 			const filterParams = event.currentTarget.value
-			const regexp = new RegExp(filterParams, "i")
-			const studentRows = document.querySelectorAll("tbody .quiz-row")
-			studentRows.forEach((studentRow) => {
-				const started = studentRow.querySelector("td.quiz-status").innerText
-				studentRow.style.display = regexp.test(started) ? "" : "none"
-			});
+			const studentRows = document.querySelectorAll(".table tbody .quiz-row")
+			if (filterParams == 'Finished') {
+				studentRows.forEach((studentRow) => {
+					const started = studentRow.querySelector("td:nth-child(2)").innerText
+					studentRow.style.display = !/-/.test(started) ? "" : "none"
+				});
+			} else {
+				const regexp = new RegExp(filterParams, "i")
+				studentRows.forEach((studentRow) => {
+					const started = studentRow.querySelector("td.quiz-status").innerText
+					studentRow.style.display = regexp.test(started) ? "" : "none"
+				});
+			}
 		}
 
 
