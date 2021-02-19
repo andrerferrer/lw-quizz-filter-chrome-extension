dd25b6a6dbe3bf23ba51a1d322ab98b001ef7964
andre ferrer <andrerferrer@hotmail.com>
2021-02-19 11:55:58 -0300

    Fix bug

--- not_ugly.js
+++ not_ugly.js
@@ -6,7 +6,7 @@ const filterTables = () => {
 			<div class="m-3">
 				<label for="filter">Which status are you looking for?</label>
 				<select id="filter">
-				  <option value="Started">All</option>
+				  <option value="All">All</option>
 				  <option value="Not Started">Not Started</option>
 				  <option value="Finished">Finished</option>
 				</select>
@@ -17,10 +17,14 @@ const filterTables = () => {
 		const filterTables = (event) => {
 			const filterParams = event.currentTarget.value
 			const studentRows = document.querySelectorAll(".table tbody .quiz-row")
-			if (filterParams == 'Finished') {
+			if (filterParams === 'Finished') {
 				studentRows.forEach((studentRow) => {
 					const started = studentRow.querySelector("td:nth-child(2)").innerText
 					studentRow.style.display = !/-/.test(started) ? "" : "none"
+				});
+			} else if (filterParams === 'All') {
+				studentRows.forEach((studentRow) => {
+					studentRow.style.display = "";
 				});
 			} else {
 				const regexp = new RegExp(filterParams, "i")
